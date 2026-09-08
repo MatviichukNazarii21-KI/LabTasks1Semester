44bcac5fb9b792369a7052bff5d441c08a3920a3
Matviichuk Nazar <nazarijmatvijcuk2@gmail.com>
2026-09-08 19:28:59 +0300

    Lab 1 done

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,3 +1,50 @@
 
 print('Hello, World')
 
+a = int(2)
+b = len('Hello, World')
+c = float(2.5)
+d = ["a", "b", "c", "d"]
+print(d)
+e = dict(a=1, b=2, c=3)
+f = str('Hello, World')
+g = bool(True)
+h = (1, 2, 3)
+print(h)
+i = {1, 2, 3, 4}
+print(i)
+
+a = 10
+b = 2
+print(a + b)
+
+a = 12
+b = 2
+print(a - b)
+
+a = 2
+b = 3
+print(a * b)
+
+a = 10
+b = 2
+print(a / b)
+
+a = 10
+b = 4
+print(a // b)
+
+a = 20
+b = 3
+print(a % b)
+
+a = 15
+b = 12
+print(a ** b)
+
+print(10 == 10)
+print(10 != 5)
+print(10 > 5)
+print(10 < 5)
+print(10 >= 10)
+print(5 <= 10)
